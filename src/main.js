8e923a63111564b41d63c7bb88f712c6f56003fb
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-09 02:06:11 +0700

    Fix bug where collate job wouldn't kick off
    If job is new (block height -1) then a number was string concatenated instead of added.

--- src/main.js
+++ src/main.js
@@ -60,7 +60,7 @@ async function main() {
   // If database is behind bitcoind, trigger collate job
   if (status.height.bitcoind > status.height.overnode) {
     const fromHeight = status.height.overnode;
-    let toHeight = fromHeight + process.env.COLLATION_JOB_CHUNK_SIZE;
+    let toHeight = fromHeight + Number(process.env.COLLATION_JOB_CHUNK_SIZE);
     if (toHeight > status.height.bitcoind) {
       toHeight = status.height.bitcoind;
     }
