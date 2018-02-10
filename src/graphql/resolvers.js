377cc97a9af0d776606c633167574e87fc84d12f
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-11 03:03:06 +0700

    Fix error on req tx's for block 0

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -25,26 +25,31 @@ const resolvers = {
       return summary;
     },
     async transactions(block, args) {
-      // Limit rows to be returned
-      const limit = args.limit || defaultResultCount;
-      if (limit > maxResultCount) {
-        throw new Error(`Please limit transactions to no more than ${maxResultCount} results.`);
-      }
-
-      // Get range of tx ids to be returned
-      const txIds = block.tx.slice(args.fromIndex, args.fromIndex + limit);
-
-      // Fetch transactions synchronously.  Async would require that we
-      // increase the limit that bitcoind will accept by setting option rpcworkqueue=<n>
-      // to a higher number than the default (16?) but obviously there's a ceiling
-      // to how far we can keepd doing that.
-      // This data will be moved to database soon(™)
       const transactions = [];
-      // eslint-disable-next-line no-restricted-syntax
-      for (const txid of txIds) {
-        // eslint-disable-next-line no-await-in-loop
-        const transaction = await resolvers.Query.transaction({}, { txid });
-        transactions.push(transaction);
+      if (block.height === 0) {
+        // bitcoind rpc method getrawtransaction doesn't work for this block
+        // TODO: Find alternative
+      } else {
+        // Limit rows to be returned
+        const limit = args.limit || defaultResultCount;
+        if (limit > maxResultCount) {
+          throw new Error(`Please limit transactions to no more than ${maxResultCount} results.`);
+        }
+
+        // Get range of tx ids to be returned
+        const txIds = block.tx.slice(args.fromIndex, args.fromIndex + limit);
+
+        // Fetch transactions synchronously.  Async would require that we
+        // increase the limit that bitcoind will accept by setting option rpcworkqueue=<n>
+        // to a higher number than the default (16?) but obviously there's a ceiling
+        // to how far we can keepd doing that.
+        // This data will be moved to database soon(™)
+        // eslint-disable-next-line no-restricted-syntax
+        for (const txid of txIds) {
+          // eslint-disable-next-line no-await-in-loop
+          const transaction = await resolvers.Query.transaction({}, { txid });
+          transactions.push(transaction);
+        }
       }
 
       return transactions;
