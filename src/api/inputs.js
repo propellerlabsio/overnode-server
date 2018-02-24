dc5f138ef1c799e3f3d1c34032d4ab41395da673
Peter Roehlen <peter.roehlen@gmail.com>
2018-02-24 16:43:49 +0700

    Add transaction inputs api

---
+++ src/api/inputs.js
@@ -0,0 +1,12 @@
+/* Ignore camel case requirement since many of the variables and arguments    */
+/* passed through unmodified from graphql queries where the sources are       */
+/* postgresql where camelcase names are not supported.                        */
+/* eslint-disable camelcase                                                   */
+
+import { knex } from '../knex';
+
+const inputs = {
+  find: ({ transaction_id }) => knex('input').where('transaction_id', transaction_id),
+};
+
+export default inputs;
