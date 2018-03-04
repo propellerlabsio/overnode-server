6f0f4612e7a94fe4022270a30c8e5a0c7579d3ef
Peter Roehlen <peter.roehlen@gmail.com>
2018-03-04 11:50:52 +0700

    Add output value to input query

--- src/api/inputs.js
+++ src/api/inputs.js
@@ -8,10 +8,15 @@ import { knex } from '../knex';
 const inputs = {
   find: ({ transaction_id, paging }) =>
     knex('input_staging')
-      .where('transaction_id', transaction_id)
-      .andWhere('input_index', '>=', paging.offset)
+      .select('input_staging.*', 'output.value as output_value')
+      .where('input_staging.transaction_id', transaction_id)
+      .andWhere('input_staging.input_index', '>=', paging.offset)
       .limit(paging.limit)
-      .orderBy('input_index'),
+      .orderBy('input_index')
+      .leftJoin('output', {
+        'output.transaction_id': 'input_staging.output_transaction_id',
+        'output.output_index': 'input_staging.output_index',
+      }),
 };
 
 export default inputs;
