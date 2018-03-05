c72af7b852141ccecbbbce8b944247232464f97d
Peter Roehlen <peter.roehlen@gmail.com>
2018-03-05 23:18:30 +0700

    Flesh out / finish basic address queries

--- src/api/inputs.js
+++ src/api/inputs.js
@@ -17,6 +17,22 @@ const inputs = {
         'output.transaction_id': 'input_staging.output_transaction_id',
         'output.output_index': 'input_staging.output_index',
       }),
+  findByAddress: ({ address, paging }) =>
+    knex('output_address')
+      .where('output_address.address', address)
+      .join('output', {
+        'output.transaction_id': 'output_address.transaction_id',
+        'output.output_index': 'output_address.output_index',
+      })
+      .join('input_staging', {
+        'output.transaction_id': 'input_staging.output_transaction_id',
+        'output.output_index': 'input_staging.output_index',
+      })
+      .select('input_staging.*', 'output.value as output_value')
+      .andWhere('input_staging.input_index', '>=', paging.offset)
+      .orderBy('output.value')
+      .limit(paging.limit)
+      .offset(paging.offset),
 };
 
 export default inputs;
--- src/api/transactions.js
+++ src/api/transactions.js
@@ -10,14 +10,6 @@ const transactions = {
     knex('transaction')
       .where('transaction_id', transaction_id)
       .first(),
-  findByAddress: ({ address, paging }) =>
-    knex('output_address')
-      .select('transaction.*')
-      .innerJoin('transaction', 'transaction.transaction_id', 'output_address.transaction_id')
-      .where('output_address.address', address)
-      .andWhere('transaction.transaction_index', '>=', paging.offset)
-      .limit(paging.limit)
-      .orderBy('transaction_index'),
   findByBlock: ({ block, paging }) =>
     knex('transaction')
       .where('block_hash', block.hash)
