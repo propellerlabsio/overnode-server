c776f33bb984051b4b0c44991aa7dd229b846763
Peter Roehlen <peter.roehlen@gmail.com>
2018-05-08 12:51:19 +0700

    Add basic profiing (total time) using winston

--- syncOneBlock.js
+++ syncOneBlock.js
@@ -1,31 +1,15 @@
-/**
- * Backwards sync any jobs that have a from-height of greater than zero.
- *
- * Normal sync operations are forward however it is useful to deploy
- * new sync jobs at a recent block height and allow users to continue
- * to use the service while a second process (this one) backwards
- * syncs back to block height 0.
- *
- * This script runs a backwards sync operation to sync the blockchain
- * to the database.  The regular main process only syncs forward and
- * does so as rapidly as possible while sacrificing service functionality.
- * This process has only resource impact (cpu, ram, disk) on the main process.
- *
- * This script should be launched as a separate node process well after
- * the server has been started with the regular start.js script and
- * the forward sync has been caught up.  At that point, the main process
- * will only be syncing new blocks foward (every 10 minutes or so) and this
- * process should have neglible impact.
- */
 
 /* Allow console messages in this module */
 /* eslint-disable no-console             */
 
+const logger = require('winston');
 // Load local environment variables first before any other action
 require('dotenv').config();
 
 const sync = require('./dist/api/sync').default;
 const rpc = require('./dist/io/rpc');
+
+logger.profile('all');
 
 // Initialize and test rpc connection
 rpc
@@ -35,7 +19,6 @@ rpc
     sync
       .getCoverage()
       .then(([{ from }, { to }]) => {
-        console.log(to);
         const nextBlock = to + 1;
         console.log(`Syncing block ${nextBlock}`);
         sync
@@ -45,7 +28,10 @@ rpc
             direction: sync.FORWARD,
           })
           .then(() => {
-            console.log('Block synced');
+            logger.profile('all');
+            logger.stream({ start: -1 }).on('log', (log) => {
+              console.log(log);
+            });
             process.exit(0);
           });
       });
