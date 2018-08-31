09458876fab462ef15dbcffc8cb6e749fe410add
Peter Roehlen <peter.roehlen@gmail.com>
2018-08-31 08:17:39 +0700

    Add real outputvalue to inputs.find; fix outputs address exception

--- src/api/inputs.js
+++ src/api/inputs.js
@@ -1,11 +1,12 @@
 // TODO remove
 /* eslint-disable arrow-body-style, camelcase, no-unused-vars, max-len */
 import transactions from './transactions';
+import outputs from './outputs';
 
 const inputs = {
   find: async ({ transaction_id, paging }) => {
     const transaction = await transactions.get({ transaction_id });
-    const result = transaction.vin
+    const results = transaction.vin
       .slice(paging.offset, (paging.offset + paging.limit))
       .map((input, index) => ({
         transaction_id,
@@ -13,10 +14,19 @@ const inputs = {
         coinbase: input.coinbase,
         output_transaction_id: input.txid,
         output_number: input.vout,
-        output_value: 9999999999, // TODO output lookup
       }));
 
-    return result;
+    // Build promises to update UTXO value in our results
+    const promises = results.map((result, index) => {
+      return outputs
+        .get({ transaction_id: result.transaction_id, output_number: result.output_number })
+        .then((output) => {
+          result.output_value = output.value;
+          return result;
+        });
+    });
+
+    return Promise.all(promises);
   },
   findByAddress: ({ address, paging }) => [],
   // knex
--- src/api/outputs.js
+++ src/api/outputs.js
@@ -3,17 +3,26 @@
 import transactions from './transactions';
 
 const outputs = {
+  get: async ({ transaction_id, output_number }) => {
+    const transaction = await transactions.get({ transaction_id });
+    return transaction.vout[output_number];
+  },
   find: async ({ transaction_id, paging }) => {
     const transaction = await transactions.get({ transaction_id });
     const result = transaction.vout
       .slice(paging.offset, (paging.offset + paging.limit))
-      .map((output, index) => ({
-        transaction_id,
-        output_number: index + paging.offset,
-        value: output.value,
-        address: output.scriptPubKey ? output.scriptPubKey.addresses[0] : '',
-        addresses: output.scriptPubKey ? output.scriptPubKey.addresses : [],
-      }));
+      .map((output, index) => {
+        const addresses = output.scriptPubKey && output.scriptPubKey.addresses ?
+          output.scriptPubKey.addresses :
+          ['???']; // TODO investigate what to do with these - possibly pay to script hash
+        return {
+          transaction_id,
+          output_number: index + paging.offset,
+          value: output.value,
+          address: addresses[0],  // TODO follow up why we are returning 2 address properties in schema
+          addresses,
+        };
+      });
     return result;
   },
   findByAddress: ({ address, paging }) => [],
